bf39d7bd2c96ba53e256cc0fc5730d2e9f3ca341
JGluhov <jgluhov@gmail.com>
2016-01-30 01:07:12 +0300

    add debugInfoEnabled

--- src/app.config.js
+++ src/app.config.js
@@ -1,9 +1,11 @@
 /**
  * Created by jgluhov on 29/01/16.
  */
-config.$inject = ['$urlRouterProvider', '$locationProvider'];
+config.$inject = ['$urlRouterProvider', '$locationProvider','$compileProvider'];
 
-export default function config($urlRouterProvider, $locationProvider) {
+export default function config($urlRouterProvider, $locationProvider, $compileProvider) {
   $locationProvider.html5Mode(true);
   $urlRouterProvider.otherwise('/');
+
+  $compileProvider.debugInfoEnabled(true);
 }
