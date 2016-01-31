79c25059ca7a0e611c48847885366239d37b35fb
JGluhov <jgluhov@gmail.com>
2016-01-31 13:19:13 +0300

    fix package.json and webpack.conf

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
     filename: "bundle.js"
   },
   resolve: {
-    extensions: ["", ".js", ".styl"]
+    extensions: ["", ".js", ".styl", ".jade"]
   },
   module: {
     loaders: [
